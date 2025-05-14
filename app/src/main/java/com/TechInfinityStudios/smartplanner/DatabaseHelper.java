1fe5acff2c7a9d5f39377aeabb48f50c32baa36b
Nicole <99645247+NicoleTrainer@users.noreply.github.com>
2025-05-14 17:43:07 -0400

    Update DatabaseHelper.java

--- app/src/main/java/com/TechInfinityStudios/smartplanner/DatabaseHelper.java
+++ app/src/main/java/com/TechInfinityStudios/smartplanner/DatabaseHelper.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     private static final String DATABASE_NAME = "planner.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     private static final String TABLE_EVENTS = "events";
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,7 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE events(id INTEGER PRIMARY KEY AUTOINCREMENT, event TEXT, time TEXT, date TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE events(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, time TEXT, date TEXT)");
         sqLiteDatabase.execSQL("CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, note TEXT)");
         sqLiteDatabase.execSQL("CREATE TABlE toDoList(id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT)");
         sqLiteDatabase.execSQL("CREATE TABLE timer(id INTEGER PRIMARY KEY AUTOINCREMENT, time TEXT)");
